5cb050f711d29039e6faaddca33c58f048528184
Ísak <isl5@hi.is>
2022-10-19 20:29:11 +0000

    task :100:

--- src/main/java/is/hi/hbv501g/group_project/task/TaskService.java
+++ src/main/java/is/hi/hbv501g/group_project/task/TaskService.java
@@ -1,17 +1,39 @@
 package is.hi.hbv501g.group_project.task;
 
 
+import is.hi.hbv501g.group_project.appuser.AppUser;
+import is.hi.hbv501g.group_project.project.AddProjectRequest;
+import is.hi.hbv501g.group_project.project.Project;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-//@AllArgsConstructor
-public interface TaskService {
+@AllArgsConstructor
+//public interface TaskService {
+public class TaskService {
 
-    // private final TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
 
-    //public void saveTask(Task task){
-    //    taskRepository.save(task);
-    //}
+    private final Project project;
+
+    private final TaskService taskService;
+
+    public void saveTask(Task task){
+        taskRepository.save(task);
+    }
+
+
+    public void saveTask(TaskServiceRequest request, AppUser user, Project project, Task task) {
+        taskService.saveTask(
+                new Task(
+                        task.getId(),
+                        project.getId(),
+                        request.getName(),
+                        user.getId(),
+                        request.getStart(),
+                        request.getDeadline(),
+                        request.getStatus())
+        );
+    }
 
 }
