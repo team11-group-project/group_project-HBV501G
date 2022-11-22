2d765b95976c4230db3c8ff66ad19f229a0abcbb
bjarkividars <bjarki.vidars@gmail.com>
2022-11-22 15:26:30 +0000

    added property source

--- src/main/java/is/hi/hbv501g/group_project/GroupProjectApplication.java
+++ src/main/java/is/hi/hbv501g/group_project/GroupProjectApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class GroupProjectApplication {
 
     /***
